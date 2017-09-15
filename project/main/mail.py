3df1df72f230a4e20c1a13a08cae055b37c031b1
JanRuettinger <jan-hendrik.ruettinger@online.de>
2017-09-15 17:00:14 +0200

    Fix #3 for URL in result email

--- project/main/mail.py
+++ project/main/mail.py
@@ -134,7 +134,7 @@ def send_email(to, subject, template, name, link_hash):
     msg = Message(subject,
                   sender=app.config['MAIL_SENDER'], recipients=[to])
     #msg.body = render_template(template + '.txt', **kwargs)
-    msg.html = render_template(template + '.html', name, link)
+    msg.html = render_template(template + '.html', name=name, link=link)
     mail.send(msg)
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
